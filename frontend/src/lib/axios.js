b8413cc3f3e6ff0c56bb24a501a14ee3496a22e8
tranthanhbao-2609 <tranthanhbao2609@gmail.com>
2025-11-19 02:36:34 +0700

    Update axios.js

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-  baseURL: import.meta.env.MODE === "development" ? "https://tieuluanlaptrinhmang.onrender.com/api" : "/api",
+  baseURL: import.meta.env.MODE === "development" ? "https://tieuluanlaptrinhmangbacken.onrender.com/api" : "/api",
   withCredentials: true,
 });
